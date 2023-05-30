19aa86209a1053ebaace4f91111b4c46dd89c51a
francoterragna <francoterragna@hotmail.com>
2023-05-30 18:09:35 -0300

    implementando los controllers

--- src/dao/models/carts.js
+++ src/dao/models/carts.js
@@ -1,16 +1,30 @@
 const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-paginate-v2');
 
 const CartSchema = mongoose.Schema({
-    products: [
-        {
+  products: [
+    {
           _id: false ,
           pid: String,
           quantity: Number
         }
-    ]
-});
+      ]
+    });
+    
+    
+    module.exports = mongoose.model('Cart', CartSchema);
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    const mongoosePaginate = require('mongoose-paginate-v2');
 
 CartSchema.plugin(mongoosePaginate);
-
-module.exports = mongoose.model('Cart', CartSchema);
