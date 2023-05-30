39929dba9907f25adc600f15625954fb1871d2cb
francoterragna <francoterragna@hotmail.com>
2023-05-30 18:12:22 -0300

    sistema con controllers

--- src/dao/models/carts.js
+++ src/dao/models/carts.js
@@ -12,30 +12,3 @@ const CartSchema = mongoose.Schema({
     
     
     module.exports = mongoose.model('Cart', CartSchema);
-    
-    
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    const mongoosePaginate = require('mongoose-paginate-v2');
-
-CartSchema.plugin(mongoosePaginate);
-  products: [
-    {
-      _id: false ,
-      pid: String,
-      quantity: Number
-    }
-  ]
-});
-
-
-module.exports = mongoose.model('Cart', CartSchema);
